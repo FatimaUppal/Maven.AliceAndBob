45b4a33f058c5529d84f6090f3f7a66370017ee2
FatimaUppal <125939989+FatimaUppal@users.noreply.github.com>
2023-03-02 20:35:43 -0500

    i have made changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,22 @@
 /**
- * Created by iyasuwatts on 10/17/17.
+ * Created by yottawatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
-    public static void main(String[] args ){
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your Name:");
 
+        String greet = input.nextLine();
+
+        if (rightName(greet)){
+            System.out.println("Welcome to the program " + greet + "!");
+        }else {
+            System.out.print("Go Away!!!!!!");
+        }input.close();
+    }
+    public static boolean rightName(String greet){
+        return greet.equalsIgnoreCase("Alice") || greet.equalsIgnoreCase("Bob");
     }
 }
